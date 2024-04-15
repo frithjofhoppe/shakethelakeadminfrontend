f502d10785475c3985952f06fa82dd29f7df85ff
Burger Alain (IT-PTR-CEN2-YPT) <alain_nicolas.burger@sbb.ch>
2024-04-15 15:40:38 +0200

    feat: setup tailwind.config.js

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,9 +5,23 @@ import Footer from '../components/footer';
 const HomePage: React.FC = () => {
     return (
         <div>
-            <Header />
-            {}
-            <Footer />
+            <Header/>
+            {
+                <>
+                    <h1> TEST H1</h1>
+                    <h2> TEST H2</h2>
+                    <h3> TEST H3</h3>
+                    <h4> TEST H4</h4>
+                    <h5> TEST H5</h5>
+                    <p className="p-xl">Test Paragraph XL</p>
+                    <p className="p-l">Test Paragraph L</p>
+                    <p className="p-m">Test Paragraph M</p>
+                    <p className="p-s">Test Paragraph S</p>
+                    <p className="p-s-light">Test Paragraph S Light</p>
+                    <p className="p-xs">Test Paragraph XS</p>
+                </>
+            }
+            <Footer/>
         </div>
     );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#004D9F',
+        secondary: '#0EC8C8',
+        text: '#000000',
+      },
+      fontFamily: {
+        sans: ['Roboto', 'sans-serif'],
+      },
+      fontSize: {
+        h1: ['46px', { lineHeight: '120%', letterSpacing: '-0.01em' }],
+        h2: ['38px', { lineHeight: '120%', letterSpacing: '-0.01em' }],
+        h3: ['28px', { lineHeight: '120%', letterSpacing: '-0.01em' }],
+        h4: ['24px', { lineHeight: '120%' }],
+        h5: ['18px', { lineHeight: '120%', letterSpacing: '0.08em' }],
+        'p-xl-light': ['24px', { lineHeight: '140%' }],
+        'p-l-light': ['18px', { lineHeight: '120%', letterSpacing: '0.01em' }],
+        'p-m-light': ['16px', { lineHeight: '120%', letterSpacing: '0.01em' }],
+        'p-s-link': ['15px', { lineHeight: '140%', letterSpacing: '0.01em' }],
+        'p-s-light': ['14px', { lineHeight: '140%', letterSpacing: '0.01em' }],
+        'p-xs-light': ['12px', { lineHeight: '120%', letterSpacing: '0.01em' }],
+      },
+      fontWeight: {
+        medium: '500',
+      }
+    },
+  },
+  plugins: [],
+}
+
