065f7f7371a92989e3551c1e277d7a3908ca6117
Burger Alain (IT-PTR-CEN2-YPT) <alain_nicolas.burger@sbb.ch>
2024-04-22 10:42:02 +0200

    feat: small styling adjustment

--- src/pages/event/event-card.tsx
+++ src/pages/event/event-card.tsx
@@ -16,7 +16,7 @@ const EventCard: React.FC<{ event: EventDto }> = ({event}) => {
     return (
         <Card className="relative w-full max-w-full">
             <div className="absolute top-2 right-2 flex space-x-2">
-                <Trash onClick={handleDelete} className="cursor-pointer mt-2 mr-2" />
+                <Trash onClick={handleDelete} className="cursor-pointer mt-2 mr-2 hover:text-red-600" />
             </div>
             <CardHeader className="flex justify-start items-start">
                 <CardTitle>{event.title}</CardTitle>
