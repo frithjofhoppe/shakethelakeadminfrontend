a3e5cafbadab445cc33e65d424b079ac177fd515
7imonn <timon.vandennobelen@novu.ch>
2024-05-01 13:31:53 +0200

    fix: now app runs with vite

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import ShakeTheLakeIcon from '../../assets/icons/shake-the-lake-white.svg?react';
+// Import ShakeTheLakeIcon from '../../assets/icons/shake-the-lake-white.svg?react';
 
 const Footer: React.FC = () => (
 	<div
 		className="w-full bg-gradient-to-r from-sky-700 via-cyan-500 to-cyan-500 flex items-center justify-start px-10 py-8 h-32 md:h-36 lg:h-40 pt text-white">
 		<div className="h-28 w-28">
-			<ShakeTheLakeIcon />
+			{/* <ShakeTheLakeIcon /> */}
 		</div>
 	</div>
 );
