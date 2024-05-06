980f07314733a74879d4d5ba4b3ea6743e8d7c33
Nadine Zbinden <nadine.zbinden@students.fhnw.ch>
2024-05-06 14:26:36 +0200

    run lint, fix some errors

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import EventList from './event/event-list';
+import {useTranslation} from 'react-i18next';
 
-const HomePage: React.FC = () => (
-	<div className="flex flex-col h-screen">
-		<div className="flex flex-col items-center overflow-y-auto pt-32 ">
-			<h1 className="text-2xl font-bold mb-10">Shake The Lake Admin</h1>
-			<EventList/>
+const HomePage: React.FC = () => {
+	const {t} = useTranslation();
+
+	return (
+		<div className="flex flex-col h-screen">
+			<div className="flex flex-col items-center overflow-y-auto pt-32 ">
+				<h1 className="text-2xl font-bold mb-10">{t('AppName')}</h1>
+          	<h4>{t('WelcomeMessage')}</h4>
+
+				<EventList/>
+			</div>
 		</div>
-	</div>
-);
+	);
+};
+
 export default HomePage;
--- src/pages/event/create-event.tsx
+++ src/pages/event/create-event.tsx
@@ -1,143 +1,145 @@
 import React from 'react';
-import {SubmitHandler, useForm} from "react-hook-form";
-import {z} from "zod";
-import {zodResolver} from "@hookform/resolvers/zod";
-import {useNavigate} from "react-router-dom";
-import {EventDto} from "../../models/api/event.model";
-import {createEvent} from "../../services/EventService";
-import {Form, FormControl, FormField, FormItem, FormLabel} from "../../components/ui/form";
+import {type SubmitHandler, useForm} from 'react-hook-form';
+import {z} from 'zod';
+import {zodResolver} from '@hookform/resolvers/zod';
+import {useNavigate} from 'react-router-dom';
+import {type EventDto} from '../../models/api/event.model';
+import {createEvent} from '../../services/EventService';
+import {Form, FormControl, FormField, FormItem, FormLabel} from '../../components/ui/form';
 import {Input} from '../../components/ui/input';
-import {Button} from "../../components/ui/button";
+import {Button} from '../../components/ui/button';
 
 const formSchema = z.object({
-        title: z.string().min(5).max(20),
-        description: z.string(),
-        date: z.string(),
-        internalEventStart: z.string(),
-        externalEventStart: z.string(),
-        location: z.string()
-    }
+	title: z.string().min(5).max(20),
+	description: z.string(),
+	date: z.string(),
+	internalEventStart: z.string(),
+	externalEventStart: z.string(),
+	location: z.string(),
+},
 );
 
 const CreateEvent: React.FC = () => {
-    const navigate = useNavigate();
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
-        mode: 'onChange'
-    });
+	const navigate = useNavigate();
+	const form = useForm<z.infer<typeof formSchema>>({
+		resolver: zodResolver(formSchema),
+		mode: 'onChange',
+	});
 
-    const onSubmit: SubmitHandler<z.infer<typeof formSchema>> = (values) => {
-        // TODO sync for design/prototyp update: how to implement further form inputs for location properties
-        const event: EventDto = {
-            id: 0,
-            title: values.title,
-            description: values.description,
-            location: {
-                id: 0,
-                name: values.location,
-                street: '',
-                streetNr: 0,
-                town: '',
-                postalCode: 0,
-                canton: ''
-            },
-            date: values.date,
-            customerCode: '',
-            employeeCode: '',
-            customerOnlyTime: '',
-            isStarted: false,
-            startedAt: '',
-            endedAt: ''
-        };
-        try {
-            const createdEvent = createEvent(event);
-            console.log('Created event:', createdEvent);
-            navigate('/');
-        } catch (error) {
-            console.error('Failed to create event:', error);
-        }
-    };
+	const onSubmit: SubmitHandler<z.infer<typeof formSchema>> = (values) => {
+		// TODO sync for design/prototyp update: how to implement further form inputs for location properties
+		const event: EventDto = {
+			id: 0,
+			title: values.title,
+			description: values.description,
+			location: {
+				id: 0,
+				name: values.location,
+				street: '',
+				streetNr: 0,
+				town: '',
+				postalCode: 0,
+				canton: '',
+			},
+			date: values.date,
+			customerCode: '',
+			employeeCode: '',
+			customerOnlyTime: '',
+			isStarted: false,
+			startedAt: '',
+			endedAt: '',
+		};
+		try {
+			const createdEvent = createEvent(event);
+			console.log('Created event:', createdEvent);
+			navigate('/');
+		} catch (error) {
+			console.error('Failed to create event:', error);
+		}
+	};
 
-    return (
-        <div className="flex items-center justify-center p-4 min-h-screen">
-            <div className="bg-white p-5 rounded-lg shadow-md w-1/2">
-                <h2 className="text-left mb-5">Create Event</h2>
-                <Form {...form}>
-                    <form className="space-y-4">
-                        <FormField name="title" control={form.control} render={({field}) => (
-                            <FormItem>
-                                <FormLabel>Title</FormLabel>
-                                <FormControl>
-                                    <Input placeholder="Sommer Event 2024" {...field}
-                                           className="input"/>
-                                </FormControl>
-                            </FormItem>
-                        )}></FormField>
-                        <FormField name="description" control={form.control}
-                                   render={({field}) => (
-                                       <FormItem>
-                                           <FormLabel>Description</FormLabel>
-                                           <FormControl>
-                                               <Input placeholder="Shake the Lake 2024" {...field}
-                                                      className="input"/>
-                                           </FormControl>
-                                       </FormItem>
-                                   )}></FormField>
+	return (
+		<div className="flex items-center justify-center p-4 min-h-screen">
+			<div className="bg-white p-5 rounded-lg shadow-md w-1/2">
+				<h2 className="text-left mb-5">Create Event</h2>
+				<Form {...form}>
+					<form className="space-y-4">
+						<FormField name="title" control={form.control} render={({field}) => (
+							<FormItem>
+								<FormLabel>Title</FormLabel>
+								<FormControl>
+									<Input placeholder="Sommer Event 2024" {...field}
+										className="input"/>
+								</FormControl>
+							</FormItem>
+						)}></FormField>
+						<FormField name="description" control={form.control}
+							render={({field}) => (
+								<FormItem>
+									<FormLabel>Description</FormLabel>
+									<FormControl>
+										<Input placeholder="Shake the Lake 2024" {...field}
+											className="input"/>
+									</FormControl>
+								</FormItem>
+							)}></FormField>
 
-                        {/*Location ist currently just a single string field for simplicity
+						{/* Location ist currently just a single string field for simplicity
                             In the Future we will need some more fields or a seperate form to fill the date for the location
                             */}
 
-                        <FormField name="location" control={form.control}
-                                   render={({field}) => (
-                                       <FormItem>
-                                           <FormLabel>Location</FormLabel>
-                                           <FormControl>
-                                               <Input {...field} className="input"/>
-                                           </FormControl>
-                                       </FormItem>
-                                   )}></FormField>
+						<FormField name="location" control={form.control}
+							render={({field}) => (
+								<FormItem>
+									<FormLabel>Location</FormLabel>
+									<FormControl>
+										<Input {...field} className="input"/>
+									</FormControl>
+								</FormItem>
+							)}></FormField>
 
-                        <FormField name="date" control={form.control} render={({field}) => (
-                            <FormItem>
-                                <FormLabel>Date</FormLabel>
-                                <FormControl>
-                                    <Input type="datetime-local" {...field} className="input"/>
-                                </FormControl>
-                            </FormItem>
-                        )}></FormField>
-                        <FormField name="internalEventStart" control={form.control}
-                                   render={({field}) => (
-                                       <FormItem>
-                                           <FormLabel>Internal Start</FormLabel>
-                                           <FormControl>
-                                               <Input type="datetime-local" {...field}
-                                                      className="input"/>
-                                           </FormControl>
-                                       </FormItem>
-                                   )}></FormField>
-                        <FormField name="externalEventStart" control={form.control}
-                                   render={({field}) => (
-                                       <FormItem>
-                                           <FormLabel>External Start</FormLabel>
-                                           <FormControl>
-                                               <Input type="datetime-local" {...field}
-                                                      className="input"/>
-                                           </FormControl>
-                                       </FormItem>
-                                   )}
-                        ></FormField>
-                    </form>
-                </Form>
-                <div className="flex justify-end mt-5 space-x-2">
-                    <Button variant="secondary" onClick={() => navigate('/')}>Cancel</Button>
-                    <Button className="bg-primary-blue text-white"
-                            disabled={!form.formState.isValid}
-                            onClick={form.handleSubmit(onSubmit)}>Save</Button>
-                </div>
-            </div>
-        </div>
-    );
+						<FormField name="date" control={form.control} render={({field}) => (
+							<FormItem>
+								<FormLabel>Date</FormLabel>
+								<FormControl>
+									<Input type="datetime-local" {...field} className="input"/>
+								</FormControl>
+							</FormItem>
+						)}></FormField>
+						<FormField name="internalEventStart" control={form.control}
+							render={({field}) => (
+								<FormItem>
+									<FormLabel>Internal Start</FormLabel>
+									<FormControl>
+										<Input type="datetime-local" {...field}
+											className="input"/>
+									</FormControl>
+								</FormItem>
+							)}></FormField>
+						<FormField name="externalEventStart" control={form.control}
+							render={({field}) => (
+								<FormItem>
+									<FormLabel>External Start</FormLabel>
+									<FormControl>
+										<Input type="datetime-local" {...field}
+											className="input"/>
+									</FormControl>
+								</FormItem>
+							)}
+						></FormField>
+					</form>
+				</Form>
+				<div className="flex justify-end mt-5 space-x-2">
+					<Button variant="secondary" onClick={() => {
+						navigate('/'); 
+					}}>Cancel</Button>
+					<Button className="bg-primary-blue text-white"
+						disabled={!form.formState.isValid}
+						onClick={form.handleSubmit(onSubmit)}>Save</Button>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default CreateEvent;
