ef2a524aa1f25df93c10d977ed9881237f09e9fc
Alain Burger <alain.burger@students.fhnw.ch>
2024-05-07 22:31:01 +0200

    feat: fix linting and update node & typescript version

--- src/assets/i18n/i18n.ts
+++ src/assets/i18n/i18n.ts
@@ -1,28 +1,28 @@
-import i18n, {Resource} from 'i18next';
+import i18n, {type Resource} from 'i18next';
 import {initReactI18next} from 'react-i18next';
 
 const resources: Resource = {
-    en: {
-        translation: {
-            "welcome_message": "Welcome to React"
-        }
-    },
-    de: {
-        translation: {
-            "welcome_message": "Willkommen bei React"
-        }
-    }
+	en: {
+		translation: {
+			welcomeMessage: 'Welcome to React',
+		},
+	},
+	de: {
+		translation: {
+			welcomeMessage: 'Willkommen bei React',
+		},
+	},
 };
 
-i18n
-    .use(initReactI18next)
-    .init({
-        resources,
-        lng: 'de',
-        keySeparator: false,
-        interpolation: {
-            escapeValue: false
-        }
-    });
+i18n.use(initReactI18next).init({
+	resources,
+	lng: 'de',
+	keySeparator: false,
+	interpolation: {
+		escapeValue: false,
+	},
+})
+	.then(() => 'obligatory for @typescript-eslint/no-floating-promises')
+	.catch(() => 'obligatory for @typescript-eslint/no-floating-promises');
 
-export default i18n;    
+export default i18n;
