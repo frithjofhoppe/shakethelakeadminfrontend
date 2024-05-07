02e046ff39aab419b1b8c289f6efb92b5ac28ba2
Burger Alain (IT-PTR-CEN2-YPT) <alain_nicolas.burger@sbb.ch>
2024-05-07 18:52:29 +0200

    feat: upgraded node version to 21 and typescript version from 4... to >5 and fixed linting issues

--- src/assets/i18n/i18n.ts
+++ src/assets/i18n/i18n.ts
@@ -1,28 +1,26 @@
-import i18n, {Resource} from 'i18next';
+import i18n, {type Resource} from 'i18next';
 import {initReactI18next} from 'react-i18next';
 
 const resources: Resource = {
-    en: {
-        translation: {
-            "welcome_message": "Welcome to React"
-        }
-    },
-    de: {
-        translation: {
-            "welcome_message": "Willkommen bei React"
-        }
-    }
+	en: {
+		translation: {
+			welcome_message: 'Welcome to React',
+		},
+	},
+	de: {
+		translation: {
+			welcome_message: 'Willkommen bei React',
+		},
+	},
 };
 
-i18n
-    .use(initReactI18next)
-    .init({
-        resources,
-        lng: 'de',
-        keySeparator: false,
-        interpolation: {
-            escapeValue: false
-        }
-    });
+i18n.use(initReactI18next).init({
+	resources,
+	lng: 'de',
+	keySeparator: false,
+	interpolation: {
+		escapeValue: false,
+	},
+});
 
-export default i18n;    
+export default i18n;
--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,17 +1,17 @@
-import {ReportHandler} from 'web-vitals';
-
 import {type ReportHandler} from 'web-vitals';
 
 const reportWebVitals = async (onPerfEntry?: ReportHandler) => {
 	if (onPerfEntry && onPerfEntry instanceof Function) {
-		// eslint-disable-next-line promise/always-return
-		await import('web-vitals').then(({getCLS, getFID, getFCP, getLCP, getTTFB}) => {
-			getCLS(onPerfEntry);
-			getFID(onPerfEntry);
-			getFCP(onPerfEntry);
-			getLCP(onPerfEntry);
-			getTTFB(onPerfEntry);
-		});
+		 
+		await import('web-vitals').then(
+			({getCLS, getFID, getFCP, getLCP, getTTFB}) => {
+				getCLS(onPerfEntry);
+				getFID(onPerfEntry);
+				getFCP(onPerfEntry);
+				getLCP(onPerfEntry);
+				getTTFB(onPerfEntry);
+			},
+		);
 	}
 };
 
